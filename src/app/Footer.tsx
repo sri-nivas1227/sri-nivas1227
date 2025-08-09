5b38c72fa5a2253dc8129c201cd63956ec73de13
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-08-09 16:40:45 -0400

    Footer Added

---
+++ src/app/Footer.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="w-full mt-6 bg-[#5ca66b]/80 text-white  py-6 flex justify-around items-center">
+      <div className="text-center">
+        <p>
+          &copy; 2025 <span className="font-bold text-lg">Srinivas Mekela</span>
+          . All rights reserved.
+        </p>
+      </div>
+      <div className="flex justify-center items-center gap-3">
+        <Link href="https://github.com/sri-nivas1227" className="">
+          GitHub
+        </Link>
+        <Link href="https://x.com/sri_nivas1227" className="">
+          Twitter
+        </Link>
+        <Link href="https://www.linkedin.com/in/sri-nivas1227" className="">
+          LinkedIn
+        </Link>
+      </div>
+    </footer>
+  );
+}
